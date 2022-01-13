88f4d2304d6622e6d03611ba005705cb448e06f7
Samuel Freitas <samuelfreitas.ao@gmail.com>
2022-01-13 23:32:11 +0100

    Creating FileController's upload|store function

--- database/migrations/2022_01_13_205515_create_files_table.php
+++ database/migrations/2022_01_13_205515_create_files_table.php
@@ -15,7 +15,7 @@ class CreateFilesTable extends Migration
     {
         Schema::create('files', function (Blueprint $table) {
             $table->string('path')->unique();
-            $table->text('description')->nullable();
+            // $table->text('description')->nullable();
             $table->string('type', 10)->comment('File type, as such: image, video, audio, archive');
             $table->unsignedBigInteger('task_id')->nullable();
             $table->timestamps();
