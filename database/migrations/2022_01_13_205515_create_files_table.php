289194f80525e83e3f956dc5afbb317bed5b293a
Samuel Freitas <samuelfreitas.ao@gmail.com>
2022-01-13 21:59:20 +0100

    Creating File task'  model, controller, migration, seeder and factory

---
+++ database/migrations/2022_01_13_205515_create_files_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->string('path')->unique();
+            $table->text('description')->nullable();
+            $table->string('type', 10)->comment('File type, as such: image, video, audio, archive');
+            $table->unsignedBigInteger('task_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('task_id')
+            ->references('id')
+            ->on('tasks')
+            ->onDelete('CASCADE')
+            ->onUpdate('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
