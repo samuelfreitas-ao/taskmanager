fe86cbee385bff20e31baf9cda002e18434bc063
Samuel Freitas <samuelfreitas.ao@gmail.com>
2022-01-15 02:27:41 +0100

    Add new route regEx to redirect ou path url to routes.tsx

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/{path}', function () {
+    return view('index');
+})->where('path', '([A-z\d\-\/_.]+)?');
