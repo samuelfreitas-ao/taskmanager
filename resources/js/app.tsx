9f9df9062068058b42f3fdecef7894548406ce43
Samuel Freitas <samuelfreitas.ao@gmail.com>
2022-01-15 02:19:57 +0100

    Create app.tsx

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,14 +1,21 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes React and other helpers. It's a great starting point while
- * building robust, powerful web applications using React + Laravel.
- */
+import React from 'react'; // importação comum em todos arquivo do react
+import ReactDOM from 'react-dom'; //react-dom indica que está a ser utilizado na web
+
+import MyRoutes from './routes';
 
 
-/**
- * Next, we will create a fresh React component instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+// JSX (JavaScript XML): Sintaxe do XML dentro do js
+function App() {
 
-require('./components/Example');
+    return (
+        <>
+            <MyRoutes />
+        </>
+    );
+}
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('app')
+);
