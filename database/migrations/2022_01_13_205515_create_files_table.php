3f79f02df68e60a6248843fc21d893df4fd3a6c9
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-01-31 18:02:34 +0100

    refactor: constrain

--- database/migrations/2022_01_13_205515_create_files_table.php
+++ database/migrations/2022_01_13_205515_create_files_table.php
@@ -6,36 +6,30 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateFilesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('files', function (Blueprint $table) {
-            $table->id();
-            $table->string('path')->unique();
-            $table->string('type', 10)->comment('File type, as such: image, video, audio, archive');
-            $table->unsignedBigInteger('task_id');
-            $table->timestamps();
-            $table->softDeletes();
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('files', function (Blueprint $table) {
+      $table->id();
+      $table->string('path')->unique();
+      $table->string('type', 10)->comment('File type, as such: image, video, audio, archive');
+      $table->foreignId('task_id')->constrained('tasks')->cascadeOnUpdate()->cascadeOnDelete();
+      $table->timestamps();
+      $table->softDeletes();
+    });
+  }
 
-            $table->foreign('task_id')
-                ->references('id')
-                ->on('tasks')
-                ->onDelete('CASCADE')
-                ->onUpdate('CASCADE');
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('files');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('files');
+  }
 }
