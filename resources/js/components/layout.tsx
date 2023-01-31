6b4ab19a614c87ed952174bfde47347840d1692a
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-01-31 14:47:59 +0100

    chore: remove unused code

--- resources/js/components/layout.tsx
+++ resources/js/components/layout.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import { BsClipboard, BsClipboardCheck, BsClipboardData, BsClipboardMinus, BsClipboardPlus, BsClipboardX } from 'react-icons/bs';
-import { FiList } from 'react-icons/fi';
+import React from 'react'
+import { BsClipboardCheck } from 'react-icons/bs'
+
 type PropsMain = React.PropsWithChildren<{ title?: string }>
-export default function Layout({ children, title }: PropsMain) {
-    return (
-        <div className='flex w-screen h-screen bg-gray-50'>
-            <div className="w-full h-full bg-no-repeat bg-cover bg-rounded-pattern">
-                <div className="flex flex-col w-full h-full">
-                    <div className=" flex flex-col items-center h-full p-4 sm:p-2 sm:px-8 sm:py-4 overflow-y-auto"> {/* this overflow-y-auto avoids a broken layout with a white part */}
-                        {/* whiteContainer */}
-                        <div className="flex flex-col w-full xl:w-4/5 h-full px-4 py-4 rounded-lg bg-white overflow-y-auto shadow p-5">
-                            {/* Title */}
-                            <div className="flex gap-2 items-center justify-center text-5xl text-center border-b pb-4 mb-4">
-                                <BsClipboardCheck className='' /> Task Manager
-                            </div>
-                            {/* Content */}
-                            {children}
-                        </div>
-                    </div>
-                </div>
+
+export default function Layout ({ children, title }: PropsMain) {
+  return (
+    <div className='flex w-screen h-screen bg-gray-50'>
+      <div className="w-full h-full bg-no-repeat bg-cover bg-rounded-pattern">
+        <div className="flex flex-col w-full h-full">
+          <div className=" flex flex-col items-center h-full p-4 sm:p-2 sm:px-8 sm:py-4 overflow-y-auto"> {/* this overflow-y-auto avoids a broken layout with a white part */}
+            {/* whiteContainer */}
+            <div className="flex flex-col w-full xl:w-4/5 h-full px-4 py-4 rounded-lg bg-white overflow-y-auto shadow p-5">
+              {/* Title */}
+              <div className="flex gap-2 items-center justify-center text-5xl text-center border-b pb-4 mb-4">
+                <BsClipboardCheck className='' /> Task Manager
+              </div>
+              {/* Content */}
+              {children}
             </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
--- resources/js/components/modal.tsx
+++ resources/js/components/modal.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
+import React from 'react'
 
-export function Modal({ children, show }: React.PropsWithChildren<{ show: boolean }>) {
-    return (
-        <>
-            {show &&
-                <div className={`fixed top-0 right-0 left-0 bottom-0 bg-black bg-opacity-50`}>
-                    <div className="w-full h-full bg-no-repeat bg-cover bg-rounded-pattern">
-                        <div className="flex flex-col w-full h-full">
-                            <div className=" flex flex-col items-center h-full p-4 sm:p-2 sm:px-8 sm:py-4 overflow-y-auto">
-                                <div className="flex flex-col  items-center w-full xl:w-4/5 h-full px-4 py-4 rounded-lg overflow-y-auto shadow p-5">
-                                    {/* <div className=""> */}
-                                    {children}
-                                    {/* </div> */}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div >
-            }
-        </>
-    )
+export function Modal ({ children, show }: React.PropsWithChildren<{ show: boolean }>) {
+  return (
+    <>
+      {show &&
+        <div className={`fixed top-0 right-0 left-0 bottom-0 bg-black bg-opacity-50`}>
+          <div className="w-full h-full bg-no-repeat bg-cover bg-rounded-pattern">
+            <div className="flex flex-col w-full h-full">
+              <div className=" flex flex-col items-center h-full p-4 sm:p-2 sm:px-8 sm:py-4 overflow-y-auto">
+                <div className="flex flex-col  items-center w-full xl:w-4/5 h-full px-4 py-4 rounded-lg overflow-y-auto shadow p-5">
+                  {/* <div className=""> */}
+                  {children}
+                  {/* </div> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div >
+      }
+    </>
+  )
 }
