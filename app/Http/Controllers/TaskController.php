2e9a2fe4ac53b949af2685c2655467578197920b
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-01-31 12:41:50 +0100

    style: format

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,174 +9,174 @@ use stdClass;
 
 class TaskController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $tasks = Task::orderby('id','desc')->get();
-        $tasks->load('files');
-        return TaskResource::collection($tasks);
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $tasks = Task::orderby('id', 'desc')->get();
+    $tasks->load('files');
+    return TaskResource::collection($tasks);
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $feedback = ['result' => false, 'message' => '', 'data' => null];
+
+    $title = trim($request->title);
+    $description = trim($request->description);
+    $status = trim($request->status);
+    $file = $request->file('file');
+    if (!$title) {
+      $feedback['message'] = 'Informe o título da tarefa.';
+    } else if (!$description) {
+      $feedback['message'] = 'Adicione uma descrição na tarefa.';
+    } else if (!$status) {
+      $feedback['message'] = 'Selecione o estado da tarefa.';
+    } else if (Task::where('title', $title)->count() > 0) {
+      $feedback['message'] = 'Já tem uma tarefa com este título.';
+    } else {
+      try {
+        $task = new Task();
+        $task->title = $title;
+        $task->description = $description;
+        $task->status = $status;
+        $task->save();
+
+        if ($file) {
+          //Uplaod and store a file in database
+          $data = new stdClass();
+          $data->file = $file;
+          $data->task_id = $task->id;
+          FileController::upload($data);
+        }
+
+        $task->load('files');
+
+        $feedback['result'] = true;
+        $feedback['message'] = 'Tarefa cadastrada com sucesso.';
+        $feedback['data'] = new TaskResource($task);
+      } catch (\Throwable $th) {
+        $feedback['message'] = 'Houve um erro ao cadastrar tarefa. Tente novamente ou contacte o administrador do sistema.';
+      }
     }
+    return response()->json($feedback);
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $feedback = ['result' => false, 'message' => '', 'data' => null];
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    $feedback = null;
+    $task = Task::find($id);
+    if ($task) {
+      $task->load('files');
+      $feedback = new TaskResource($task);
+    } else {
+      $feedback = response()->json(['result' => false, 'message' => 'Tarefa não encontrada.', 'data' => null]);
+    }
+    return $feedback;
+  }
 
-        $title = trim($request->title);
-        $description = trim($request->description);
-        $status = trim($request->status);
-        $file = $request->file('file');
-        if (!$title) {
-            $feedback['message'] = 'Informe o título da tarefa.';
-        } else if (!$description) {
-            $feedback['message'] = 'Adicione uma descrição na tarefa.';
-        } else if (!$status) {
-            $feedback['message'] = 'Selecione o estado da tarefa.';
-        } else if (Task::where('title', $title)->count() > 0) {
-            $feedback['message'] = 'Já tem uma tarefa com este título.';
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    $feedback = ['result' => false, 'message' => '', 'data' => null];
+
+    $title = trim($request->title);
+    $description = trim($request->description);
+    $status = trim($request->status);
+    $file = $request->file;
+    if (!$title) {
+      $feedback['message'] = 'Informe o título da tarefa.';
+    } else if (!$description) {
+      $feedback['message'] = 'Adicione uma descrição na tarefa.';
+    } else if (!$status) {
+      $feedback['message'] = 'Selecione o estado da tarefa.';
+    } else if (!$task = Task::find($id)) {
+      $feedback['message'] = 'Tarefa não encontrada.';
+    } else if (Task::where('title', $title)->where('id', '<>', $id)->count() > 0) {
+      $feedback['message'] = 'Já tem uma tarefa com este título.';
+    } else {
+      try {
+        $task->title = $title;
+        $task->description = $description;
+        $task->status = $status;
+        $task->save();
+
+        if ($file) {
+          //Uplaod and store a file in database
+          $data = new stdClass();
+          $data->file = $file;
+          $data->task_id = $task->id;
+          FileController::upload($data);
+        }
+
+        $task->load('files');
+
+        $feedback['result'] = true;
+        $feedback['message'] = 'Tarefa actualizada com sucesso.';
+        $feedback['data'] = new TaskResource($task);
+      } catch (\Throwable $th) {
+        $feedback['message'] = 'Houve um erro ao actualizar tarefa. Tente novamente ou contacte o administrador do sistema.';
+      }
+    }
+    return response()->json($feedback);
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    $feedback = ['result' => false, 'message' => '', 'data' => null];
+
+    $task = Task::find($id);
+    $softDelete = request()->query('soft');
+    // $softDelete = $softDelete || $softDelete == 'true' || $softDelete == '1';
+    if (!$task) {
+      $feedback['message'] = 'Tarefa não encontrada.';
+    } else {
+      try {
+        $files = $task->files;
+        if ($softDelete) {
+          $task->delete();
         } else {
-            try {
-                $task = new Task();
-                $task->title = $title;
-                $task->description = $description;
-                $task->status = $status;
-                $task->save();
-
-                if ($file) {
-                    //Uplaod and store a file in database
-                    $data = new stdClass();
-                    $data->file = $file;
-                    $data->task_id = $task->id;
-                    FileController::upload($data);
-                }
-
-                $task->load('files');
-
-                $feedback['result'] = true;
-                $feedback['message'] = 'Tarefa cadastrada com sucesso.';
-                $feedback['data'] = new TaskResource($task);
-            } catch (\Throwable $th) {
-                $feedback['message'] = 'Houve um erro ao cadastrar tarefa. Tente novamente ou contacte o administrador do sistema.';
+          $task->forceDelete();
+          if ($files) {
+            foreach ($files as $f) {
+              FileController::removeUpload($f);
             }
+          }
         }
-        return response()->json($feedback);
+
+        $feedback['result'] = true;
+        $feedback['message'] = 'Tarefa excluída com sucesso.';
+      } catch (\Throwable $th) {
+        $feedback['message'] = 'Houve um erro ao excluir tarefa. Tente novamente ou contacte o administrador do sistema.' . $th;
+      }
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        $feedback = null;
-        $task = Task::find($id);
-        if ($task) {
-            $task->load('files');
-            $feedback = new TaskResource($task);
-        } else {
-            $feedback = response()->json(['result' => false, 'message' => 'Tarefa não encontrada.', 'data' => null]);
-        }
-        return $feedback;
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $feedback = ['result' => false, 'message' => '', 'data' => null];
-
-        $title = trim($request->title);
-        $description = trim($request->description);
-        $status = trim($request->status);
-        $file = $request->file;
-        if (!$title) {
-            $feedback['message'] = 'Informe o título da tarefa.';
-        } else if (!$description) {
-            $feedback['message'] = 'Adicione uma descrição na tarefa.';
-        } else if (!$status) {
-            $feedback['message'] = 'Selecione o estado da tarefa.';
-        } else if (!$task = Task::find($id)) {
-            $feedback['message'] = 'Tarefa não encontrada.';
-        } else if (Task::where('title', $title)->where('id', '<>', $id)->count() > 0) {
-            $feedback['message'] = 'Já tem uma tarefa com este título.';
-        } else {
-            try {
-                $task->title = $title;
-                $task->description = $description;
-                $task->status = $status;
-                $task->save();
-
-                if ($file) {
-                    //Uplaod and store a file in database
-                    $data = new stdClass();
-                    $data->file = $file;
-                    $data->task_id = $task->id;
-                    FileController::upload($data);
-                }
-
-                $task->load('files');
-
-                $feedback['result'] = true;
-                $feedback['message'] = 'Tarefa actualizada com sucesso.';
-                $feedback['data'] = new TaskResource($task);
-            } catch (\Throwable $th) {
-                $feedback['message'] = 'Houve um erro ao actualizar tarefa. Tente novamente ou contacte o administrador do sistema.';
-            }
-        }
-        return response()->json($feedback);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $feedback = ['result' => false, 'message' => '', 'data' => null];
-
-        $task = Task::find($id);
-        $softDelete = request()->query('soft');
-        // $softDelete = $softDelete || $softDelete == 'true' || $softDelete == '1';
-        if (!$task) {
-            $feedback['message'] = 'Tarefa não encontrada.';
-        } else {
-            try {
-                $files = $task->files;
-                if ($softDelete) {
-                    $task->delete();
-                } else {
-                    $task->forceDelete();
-                    if ($files) {
-                        foreach ($files as $f) {
-                            FileController::removeUpload($f);
-                        }
-                    }
-                }
-
-                $feedback['result'] = true;
-                $feedback['message'] = 'Tarefa excluída com sucesso.';
-            } catch (\Throwable $th) {
-                $feedback['message'] = 'Houve um erro ao excluir tarefa. Tente novamente ou contacte o administrador do sistema.' . $th;
-            }
-        }
-        return response()->json($feedback);
-    }
+    return response()->json($feedback);
+  }
 }
