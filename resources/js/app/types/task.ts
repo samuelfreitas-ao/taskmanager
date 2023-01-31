2d215a910a113286dc91e544e4414d6ff4b16080
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-01-31 14:48:21 +0100

    refactor: improve status type

--- resources/js/app/types/task.ts
+++ resources/js/app/types/task.ts
@@ -1,12 +1,12 @@
-import { IFile } from "./file";
+import { IFile } from './file'
 
 export type ITask = {
-    id: number
-    title: string
-    description: string
-    status: string
-    created_at: Date
-    updated_at: Date
-    deleted_at: Date
-    files: IFile[]
+  id: number
+  title: string
+  description: string
+  status: 'Activo' | 'Pendente' | 'Feito'
+  created_at: Date
+  updated_at: Date
+  deleted_at: Date
+  files: IFile[]
 }
