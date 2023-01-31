531131ee65cfe7b66549bfab09652bf6f417ddd1
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-01-31 18:02:59 +0100

    fix: add correct class helper

--- app/Http/Resources/FileResource.php
+++ app/Http/Resources/FileResource.php
@@ -2,29 +2,29 @@
 
 namespace App\Http\Resources;
 
-use App\Http\Controllers\FileController;
+use App\Helpers\FileHelper;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class FileResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
-    public function toArray($request)
-    {
-        return [
-            'id' => $this->id,
-            'path' =>$this->path,
-            'url' =>FileController::uploadLink($this->path),
-            'type' => $this->type,
-            'task_id' => $this->task_id,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-            'deleted_at' => $this->deleted_at,
-            'task' => new TaskResource($this->whenLoaded('task')),
-        ];
-    }
+  /**
+   * Transform the resource into an array.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+   */
+  public function toArray($request)
+  {
+    return [
+      'id' => $this->id,
+      'path' => $this->path,
+      'url' => FileHelper::uploadLink($this->path),
+      'type' => $this->type,
+      'task_id' => $this->task_id,
+      'created_at' => $this->created_at,
+      'updated_at' => $this->updated_at,
+      'deleted_at' => $this->deleted_at,
+      'task' => new TaskResource($this->whenLoaded('task')),
+    ];
+  }
 }
