95638e31389b7e2f211ec4ec96560775089333dd
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-01 19:30:03 +0100

    style: format

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,19 +2,19 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="logo" content="{{url('/assets/img/logo.png')}}">
-    <meta name="logo_vertical" content="{{url('/assets/img/logo-vertical.png')}}">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title class="title">Task Manager</title>
-    <link rel="stylesheet" href="css/app.css">
-    <meta name="base_url" content="{{ url('') }}">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="logo" content="{{url('/assets/img/logo.png')}}">
+  <meta name="logo_vertical" content="{{url('/assets/img/logo-vertical.png')}}">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title class="title">Task Manager</title>
+  <link rel="stylesheet" href="css/app.css">
+  <meta name="base_url" content="{{ url('') }}">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 
 <body>
-    <div class="" id="app"></div>
+  <div class="" id="app"></div>
 </body>
 
 
