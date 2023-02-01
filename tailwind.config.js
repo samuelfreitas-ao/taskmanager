0933e9002f408b9befb579a3fda1a25e2cbb6087
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-01 17:23:26 +0100

    chore: update tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    purge: [
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.tsx',
-        './resources/**/*.vue',
-    ],
-    theme: {
-        extend: {
-            width:{
-                128:'32rem'
-            }
-        },
-    },
-    variants: {
-        extend: {
-            opacity: ['disabled'],
-            backgroundColor: ['disabled'],
-            cursor: ['hover', 'focus', 'disabled'],
-            borderWidth: ['hover', 'focus'],
-            borderColor: ['responsive', 'group-hover', 'focus-within', 'hover', 'focus', 'group-focus'],
-            borderOpacity: ['responsive', 'group-hover', 'focus-within', 'hover', 'focus', 'group-focus'],
-        },
-        backgroundColor: ['responsive', 'group-hover', 'focus-within', 'hover', 'focus', 'group-focus'],
-        boxShadow: ['responsive', 'group-hover', 'focus-within', 'hover', 'focus', 'group-focus'],
-        opacity: ['disabled', 'hover', 'group-hover'],
-        scale: ['hover', 'group-hover'],
-        translate: ['hover', 'group-hover'],
-        transitionProperty: ['hover', 'group-hover', 'focus'],
-        width: ['hover', 'group-hover', 'focus'],
-    },
-    plugins: [],
+  content: ['./resources/**/*.{tsx,blade.php}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
