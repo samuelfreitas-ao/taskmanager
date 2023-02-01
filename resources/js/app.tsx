3d0e60a14e42af5fa96642b239e8d54d23bbcd31
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-01 17:23:49 +0100

    refactor: update react call

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,21 +1,5 @@
-import React from 'react'; // importação comum em todos arquivo do react
-import ReactDOM from 'react-dom'; //react-dom indica que está a ser utilizado na web
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import MyRoutes from './routes'
 
-import MyRoutes from './routes';
-
-
-// JSX (JavaScript XML): Sintaxe do XML dentro do js
-function App() {
-
-    return (
-        <>
-            <MyRoutes />
-        </>
-    );
-}
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('app')
-);
+createRoot(document.getElementById('app') as HTMLDivElement).render(<MyRoutes />)
