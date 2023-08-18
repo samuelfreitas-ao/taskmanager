26eed1038d63c02019f2fae5f871b9c7af5ea4e5
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-08-18 18:33:33 +0100

    refactor: Correct names

--- resources/js/presentation/components/task-board-card.tsx
+++ resources/js/presentation/components/task-board-card.tsx
@@ -6,25 +6,24 @@ import TaskList, { TaskTitle } from './task-list'
 type Props = {
   tasks: ITask[]
 }
-export default function TaskBoradCard (data: Props) {
-
-  const taskPending = data.tasks.filter(task => task.status === 'Pendente')
-  const taskActive = data.tasks.filter(task => task.status === 'Activo')
-  const taskDone = data.tasks.filter(task => task.status === 'Feito')
+export default function TaskBoardCard(data: Props) {
+  const pendingTasks = data.tasks.filter((task) => task.status === 'Pendente')
+  const activeTasks = data.tasks.filter((task) => task.status === 'Activo')
+  const doneTasks = data.tasks.filter((task) => task.status === 'Feito')
 
   return (
     <ul className="grid xl:grid-cols-3 lg:grid-cols-2 gap-2">
       <li className="pending bg-gray-50">
-        <TaskTitle text="Pendente" total={taskPending.length} />
-        <TaskList tasks={taskPending} />
+        <TaskTitle text="Pendente" total={pendingTasks.length} />
+        <TaskList tasks={pendingTasks} />
       </li>
       <li className="active bg-gray-50">
-        <TaskTitle text="Activo" total={taskActive.length} />
-        <TaskList tasks={taskActive} />
+        <TaskTitle text="Activo" total={activeTasks.length} />
+        <TaskList tasks={activeTasks} />
       </li>
       <li className="done bg-gray-50">
-        <TaskTitle text="Feito" total={taskDone.length} />
-        <TaskList tasks={taskDone} />
+        <TaskTitle text="Feito" total={doneTasks.length} />
+        <TaskList tasks={doneTasks} />
       </li>
     </ul>
   )
