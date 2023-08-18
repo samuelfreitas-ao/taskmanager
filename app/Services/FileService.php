df89f2ddc85fb791edf9b7bf4809503e6edfc412
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-08-18 20:11:53 +0100

    refactor: correct text case

--- app/Services/FileService.php
+++ app/Services/FileService.php
@@ -8,27 +8,27 @@ use Illuminate\Http\Request;
 
 class FileService
 {
-  public function store(Request $request): bool
-  {
-    $file = $request->file;
-    $taskId = $request->task_id;
+	public function store(Request $request): bool
+	{
+		$file = $request->file;
+		$taskId = $request->task_id;
 
-    $filePath = FileHelper::upload($file);
+		$filePath = FileHelper::upload($file);
 
-    $paths = is_array($filePath) ? $filePath : [$filePath];
-    $data = [];
-    foreach ($paths as $path) {
-      $data[] = ['task_id' => $taskId, 'path' => $path, 'type' => FileHelper::getType($path), 'created_At' => now()];
-    }
+		$paths = is_array($filePath) ? $filePath : [$filePath];
+		$data = [];
+		foreach ($paths as $path) {
+			$data[] = ['task_id' => $taskId, 'path' => $path, 'type' => FileHelper::getType($path), 'created_at' => now()];
+		}
 
-    if (count($data) < 0) return false;
+		if (count($data) < 0) return false;
 
-    File::insert($data);
-    return true;
-  }
+		File::insert($data);
+		return true;
+	}
 
-  public function deleteFiles(string | array $filePath): void
-  {
-    FileHelper::removeUpload(is_array($filePath) ? $filePath : [$filePath]);
-  }
+	public function deleteFiles(string | array $filePath): void
+	{
+		FileHelper::removeUpload(is_array($filePath) ? $filePath : [$filePath]);
+	}
 }
